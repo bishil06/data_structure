7dc89b472e84dc1203364b968bdcfb20fb472662
bishil06 <bishil06@naver.com>
2021-02-03 00:37:00 +0900

    add linked list node test
    create test
    set value test
    get value test
    set next test
    get next test

--- src/linked_list/__tests__/linkedListNode.test.js
+++ src/linked_list/__tests__/linkedListNode.test.js
@@ -1,14 +1,14 @@
 const LinkedListNode = require('../linkedListNode.js');
 
 describe('Linked List Node', () => {
-    test('should create empty Linked List', () => {
+    test('should create empty Linked List Node', () => {
         const node = new LinkedListNode();
 
         expect(node.value).toBeNull();
         expect(node.nextNode).toBeNull();
     });
 
-    test('should create Linked List with value', () => {
+    test('should create Linked List Node with value', () => {
         const v = 1;
         const node = new LinkedListNode(v);
 
@@ -16,15 +16,64 @@ describe('Linked List Node', () => {
         expect(node.nextNode).toBeNull();
     });
 
-    test('should create Linked List with value, nextNode', () => {
+    test('should create Linked List Node with value, nextNode', () => {
         const node1 = new LinkedListNode(1);
         const node2 = new LinkedListNode(2, node1);
         
         expect(node1.value).toBe(1);
         expect(node1.nextNode).toBeNull();
-        
+
         expect(node2.value).toBe(2);
         expect(node2.nextNode.value).toBe(1);
         expect(node2.nextNode.nextNode).toBeNull();
+    });
+
+    test('should set value Node', () => {
+        const node = new LinkedListNode(1);
+
+        expect(node.value).toBe(1);
+        node.setValue(10);
+        expect(node.value).toBe(10);
+    });
+
+    test('should get value Node', () => {
+        const node = new LinkedListNode(1);
+
+        expect(node.getValue()).toBe(1);
+    });
+
+    test('should set next node', () => {
+        const node1 = new LinkedListNode(1);
+        const node2 = new LinkedListNode(2);
+        const node3 = new LinkedListNode(3);
+
+        node1.setNext(node2);
+        expect(node1.nextNode.getValue()).toBe(2);
+        node1.setNext(node3);
+        expect(node1.nextNode.getValue()).toBe(3);
+    });
+
+    test('should get next node', () => {
+        const node1 = new LinkedListNode(1);
+        const node2 = new LinkedListNode(2);
+        const node3 = new LinkedListNode(3);
+
+        node1.setNext(node2);
+        expect(node1.getNext().getValue()).toBe(2);
+        node1.setNext(node3);
+        expect(node1.getNext().getValue()).toBe(3);
+    });
+
+    test('should iterate node', () => {
+        const node1 = new LinkedListNode(1);
+        const node2 = new LinkedListNode(2);
+        const node3 = new LinkedListNode(3);
+
+        node1.setNext(node2);
+        node2.setNext(node3);
+        
+        expect(node1.getValue()).toBe(1);
+        expect(node1.getNext().getValue()).toBe(2);
+        expect(node1.getNext().getNext().getValue()).toBe(3);
     });
 });
--- src/linked_list/linkedListNode.js
+++ src/linked_list/linkedListNode.js
@@ -3,6 +3,22 @@ class LinkedListNode {
         this.value = value ? value : null;
         this.nextNode = nextNode ? nextNode : null;
     }
+
+    setValue(value) {
+        this.value = value;
+    }
+
+    getValue(value) {
+        return this.value;
+    }
+
+    setNext(node) {
+        this.nextNode = node;
+    }
+
+    getNext(node) {
+        return this.nextNode;
+    }
 }
 
 module.exports = LinkedListNode;
