a69af7188c6807f93ae7685a19343e93f803e795
bishil06 <bishil06@naver.com>
2021-02-03 00:40:23 +0900

    create linked list
    create linked list

---
+++ src/linked_list/linkedList.js
@@ -0,0 +1,11 @@
+const LinkedListNode = require('./linkedListNode.js');
+
+class LinkedList {
+    constructor(iter) {
+        this.head = null;
+        this.tail = null;
+        this.cur = null;
+        this.numOfNode = 0;
+        this.compFn = null;
+    }
+}
